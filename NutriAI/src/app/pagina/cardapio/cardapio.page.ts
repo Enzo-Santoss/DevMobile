36a02d2f66db2bed51b3b664a3919c93fc11dfed
Enzo Santos <enzo.santoss@hotmail.com>
2025-09-11 03:13:35 -0300

    atualizado 11set
    atualizado 11set

--- NutriAI/src/app/pagina/cardapio/cardapio.page.ts
+++ NutriAI/src/app/pagina/cardapio/cardapio.page.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonMenuButton,} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonMenuButton, IonSegment, IonSegmentButton, IonLabel, IonSegmentView, IonSegmentContent, IonIcon, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonThumbnail,} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { barbell, basket, call, globe, heart, home, man, person, pin, star, trash } from 'ionicons/icons';
 
 @Component({
   selector: 'app-cardapio',
   templateUrl: './cardapio.page.html',
   styleUrls: ['./cardapio.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonMenuButton,]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonMenuButton,IonSegment,IonSegmentButton,IonLabel,IonSegmentView,IonSegmentContent,IonIcon,IonList,IonCard,IonCardContent,IonCardHeader,IonCardSubtitle,IonCardTitle,IonItem,IonThumbnail,]
 })
 export class CardapioPage implements OnInit {
 
   constructor() { }
 
+  escolhar = 'ganhar';
+
+  alterar(evento: any){
+    this.escolhar = evento.detail.value;
+
+  }
+
   ngOnInit() {
+
+  addIcons({ barbell, basket, call, globe, heart, home, person, pin, star, trash,man });
+  
   }
 
 }
