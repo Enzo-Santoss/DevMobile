e42a3abe14858f4a2fd1af74ddc028222accb55b
Jorge <mail.jorgejacob@gmail.com>
2025-11-07 01:26:10 -0300

    Perfil
    faltava uma virgula...

--- NutriAI/src/environments/environment.ts
+++ NutriAI/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: "AIzaSyCf4drKIUHOWvruK-5xrs6O9dvC3KDHu0g",
     authDomain: "nutriai-f15af.firebaseapp.com",
     projectId: "nutriai-f15af",
-    storageBucket: "nutriai-f15af.appspot.com"
+    storageBucket: "nutriai-f15af.appspot.com",
     messagingSenderId: "288409526883",
     appId: "1:288409526883:web:c7215e020eb45ea9ed8e56"
   }
